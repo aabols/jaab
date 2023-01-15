3a851fe0d69d9c353814c9f5d8cc9dce3ead4a8b
jaab <trakaisjanka@gmail.com>
2023-01-15 23:54:04 +0000

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import Users from './components/users';
 
 function App() {
   return (
     <div className="App">
-      <Users/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
