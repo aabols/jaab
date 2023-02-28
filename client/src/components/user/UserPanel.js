230b0cd1eeb35edbdd880c66db8cd39f9ea0c165
jaab <trakaisjanka@gmail.com>
2023-02-28 00:55:13 +0000

    deployment action test

--- client/src/components/user/UserPanel.js
+++ client/src/components/user/UserPanel.js
@@ -37,7 +37,7 @@ export default function UserPanel() {
                 <input
                     type='button'
                     style={{ backgroundColor: deleteConfirm ? '#f66' : '#faa' }}
-                    value={deleteConfirm ? 'Confirm account deletion' : 'Delete account'}
+                    value={deleteConfirm ? 'Confirm account delete' : 'Delete account'}
                     onClick={handleDelete}
                 />
             </div>
