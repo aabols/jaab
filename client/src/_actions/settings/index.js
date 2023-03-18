29d0ba77af45399e946c30e513315b496e6d1dba
jaab <trakaisjanka@gmail.com>
2023-03-18 00:08:32 +0000

    persistent list refresh delay setting

--- client/src/_actions/settings/index.js
+++ client/src/_actions/settings/index.js
@@ -1,6 +1,18 @@
+import { settingsConstants } from "../../_constants/settingsConstants";
 import { themeConstants } from "../../_constants/themeConstants";
 
 export const settingsActions = {
+    setRefreshDelay: (delay) => dispatch => {
+        localStorage.setItem(
+            settingsConstants.LOCAL_STORAGE_KEY_REFRESH_DELAY,
+            delay
+        );
+        return dispatch({
+            type: settingsConstants.SET_REFRESH_RATE,
+            delay
+        });
+    },
+
     setDarkTheme: () => dispatch => {
         localStorage.setItem(
             themeConstants.LOCAL_STORAGE_KEY,
--- client/src/_reducers/settings/index.js
+++ client/src/_reducers/settings/index.js
@@ -1,9 +1,11 @@
 import { settingsConstants } from '../../_constants/settingsConstants';
 import { sortOptions } from '../../utils/sortFunctions';
 
+const listRefreshDelay = localStorage.getItem(settingsConstants.LOCAL_STORAGE_KEY_REFRESH_DELAY) || settingsConstants.DEFAULT_REFRESH_DELAY;
+
 const initialState = {
+    listRefreshDelay,
     listsRefreshDelay: 5000,
-    listRefreshDelay: 2000,
     sortOption: Object.keys(sortOptions)[0],
     shoppingMode: false,
     globalSearch: '',
@@ -11,20 +13,25 @@ const initialState = {
 
 export const settingsReducer = (state = initialState, action) => {
     switch (action.type) {
+        case settingsConstants.SET_REFRESH_RATE:
+            return {
+                ...state,
+                listRefreshDelay: Math.max(action.delay, settingsConstants.MIN_REFRESH_DELAY),
+            };
         case settingsConstants.SET_GLOBAL_SEARCH:
             return {
                 ...state,
-                globalSearch: action.payload
+                globalSearch: action.payload,
             };
         case settingsConstants.SET_SORT_OPTION:
             return {
                 ...state,
-                sortOption: action.payload
+                sortOption: action.payload,
             };
         case settingsConstants.SET_SHOPPING_MODE:
             return {
                 ...state,
-                shoppingMode: action.payload
+                shoppingMode: action.payload,
             };
         default:
             return state;
