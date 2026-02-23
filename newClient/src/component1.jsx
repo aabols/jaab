dd57548ad3528090d8c38c2c393250ae0f17b54b
jaab <trakaisjanka@gmail.com>
2026-02-23 10:38:39 +0000

    remove code splitting test components

--- newClient/src/component1.jsx
+++
@@ -1,6 +0,0 @@
-
-export default function Component1() {
-    return (
-        <div>component1</div>
-    )
-}
--- newClient/src/component2.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-export default function Component2() {
-    return (
-        <div>component2</div>
-    )
-}
