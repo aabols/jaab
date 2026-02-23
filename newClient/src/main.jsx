657ac18eef856c384c9defffa3fe7fc275705d91
jaab <trakaisjanka@gmail.com>
2026-02-23 10:38:01 +0000

    remove bootstrap, use tailwind

--- newClient/src/main.jsx
+++ newClient/src/main.jsx
@@ -1,19 +1,9 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
-const darkPreference = window.matchMedia('(prefers-color-scheme: dark)').matches;
-document.documentElement.setAttribute('data-bs-theme', darkPreference ? 'dark' : 'light');
-
-const Component1 = React.lazy(() => import('./component1'));
-import Component2 from './component2';
 
 const router = createBrowserRouter([
-    { path: '', element: <div>Blank</div> },
-    { path: 'path1', element: <Suspense><Component1 /></Suspense> },
-    { path: 'path2', element: <Component2 /> },
+    { path: '', element: <div>Hello World</div> },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
