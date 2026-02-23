2b504862f0d1e3058e87641b4d428db78a26e9bb
jaab <trakaisjanka@gmail.com>
2026-02-23 00:10:00 +0000

    initialise bootstrap

--- newClient/src/main.jsx
+++ newClient/src/main.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+const darkPreference = window.matchMedia('(prefers-color-scheme: dark)').matches;
+document.documentElement.setAttribute('data-bs-theme', darkPreference ? 'dark' : 'light');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
